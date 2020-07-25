f733384f687d0f4815398c5c5543513ccf424999
danielandersondasilva <danielandersondasilva>
2020-07-25 19:00:05 -0300

    finalizando aula 3

---
+++ imagens.js
@@ -0,0 +1,15 @@
+//imagens do jogo
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+function preload() {
+  imagemDaEstrada = loadImage("imagem/estrada.png");
+  imagemDoAtor = loadImage("imagem/ator-1.png");
+  imagemCarro = loadImage("imagem/carro-1.png");
+  imagemCarro2 = loadImage("imagem/carro-2.png");
+  imagemCarro3 = loadImage("imagem/carro-3.png");
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3];
+}
--- sketch.js
+++ sketch.js
@@ -1,16 +1,3 @@
-let imagemDaEstrada;
-let imagemDoAtor;
-let imagemCarro;
-//carro
-let xCarro = 600;
-//ator
-let yAtor = 366;
-function preload() {
-  imagemDaEstrada = loadImage("imagem/estrada.png");
-  imagemDoAtor = loadImage("imagem/ator-1.png");
-  imagemCarro = loadImage("imagem/carro-1.png");
-}
-
 function setup() {
   createCanvas(500, 400);
 }
@@ -21,25 +8,5 @@ function draw() {
   mostraCarro();
   movimentaCarro();
   movimentaAtor();
-}
-
-function mostraAtor() {
-  image(imagemDoAtor, 100, yAtor, 30, 30);
-}
-
-function mostraCarro() {
-  image(imagemCarro, xCarro, 40, 50, 40);
-}
-
-function movimentaCarro() {
-  xCarro -= 2;
-}
-
-function movimentaAtor() {
-  if (keyIsDown(UP_ARROW)) {
-    yAtor -= 3;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    yAtor += 3;
-  }
+  voltaPosicaoInicialDoCarro();
 }
