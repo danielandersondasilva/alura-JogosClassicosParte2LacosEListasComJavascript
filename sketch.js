72764b45ff90cc7363541fdcd6219099b804ce98
Daniel Anderson <danielandersondasilva@gmail.com>
2020-07-25 12:53:09 -0300

    finalzando aula1.2

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let imagemDaEstrada;
 let imagemDoAtor;
+let imagemCarro;
 
 function preload() {
   imagemDaEstrada = loadImage("imagem/estrada.png");
   imagemDoAtor = loadImage("imagem/ator-1.png");
+  imagemCarro = loadImage('imagem/carro-1.png')
 }
 
 function setup() {
@@ -13,4 +15,5 @@ function setup() {
 function draw() {
   background(imagemDaEstrada);
   image(imagemDoAtor, 100, 366, 30, 30);
+  image(imagemCarro, 420,40,50,40)
 }
